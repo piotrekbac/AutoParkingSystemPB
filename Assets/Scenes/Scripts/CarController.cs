b1f2137eb4d8424a993e01cf3ccb383e075effe8
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-14 23:03:56 +0100

    Inicjalizacja zawartości pliku CarController.cs wraz  z odpowiednimi referencjami do konkretnych komponentów

--- Assets/Scenes/Scripts/CarController.cs
+++ Assets/Scenes/Scripts/CarController.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
+// Piotr Bacior 15 722 - WSEI Kraków - Informatyka stosowana
+
 public class CarController
 {
-    
+    /* 
+     
+     Tworzê skrypt, który bêdzie odpowiada³ za sterowanie samochodem.
+     Bêdzie on wykorzystywa³ komponent Rigidbody do poruszania siê
+     samochodu oraz Transform do ustawiania œrodka masy. 
+
+    */
+
+    // Referencja do komponentu Rigidbody, który bêdzie odpowiada³ za fizykê samochodu.
+    private Rigidbody rb;
+
+    // Referencja do Transform, który bêdzie odpowiada³ za ustawianie œrodka masy samochodu.
+    public Transform centerOfMass;
 }

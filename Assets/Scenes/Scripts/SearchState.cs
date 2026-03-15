1910aac105919114cd636f0c8fe50698bb0ed75d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:35:38 +0100

    inicjalizacja implementacji metody UpdateState z interfejsu ICarState

--- Assets/Scenes/Scripts/SearchState.cs
+++ Assets/Scenes/Scripts/SearchState.cs
@@ -10,4 +10,12 @@ public class SearchState : ICarState
     {
         Debug.Log("FSM: Rozpoczynam poszukiwanie miejsca...");
     }
+
+    // Implementacja metody UpdateState z interfejsu ICarState.
+    // Ta metoda jest wywo³ywana w ka¿dej klatce, gdy samochód znajduje siê w stanie poszukiwania miejsca parkingowego.
+    // Tutaj mo¿na dodaæ logikê, która bêdzie wykonywana podczas tego stanu, np. poruszanie siê po parkingu, skanowanie otoczenia itp.
+    public void UpdateState(CarController car)
+    {
+        
+    }
 }

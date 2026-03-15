b3b2faab58a895300330fde24fee25918b2332dd
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:34:43 +0100

    dziedziczenie po ICarState oraz implementacja metody Enter

--- Assets/Scenes/Scripts/SearchState.cs
+++ Assets/Scenes/Scripts/SearchState.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 // Piotr Bacior 15 722 - WSEI Kraków - Informatyka stosowana
 
-public class SearchState
+// Nasza klasa SearchState dziedziczy po interfejsie ICarState
+public class SearchState : ICarState
 {
-    
+    // Implementacja metody Enter z interfejsu ICarState. Ta metoda jest wywo³ywana, gdy samochód wchodzi w stan poszukiwania miejsca parkingowego.
+    public void Enter(CarController car)
+    {
+        Debug.Log("FSM: Rozpoczynam poszukiwanie miejsca...");
+    }
 }

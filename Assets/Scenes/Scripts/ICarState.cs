5cf09929df07053689f15394b19e9677e1b297f8
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:28:11 +0100

    metoda Exit wraz z odpowiednimi komentarzami ją tłumączącymi

--- Assets/Scenes/Scripts/ICarState.cs
+++ Assets/Scenes/Scripts/ICarState.cs
@@ -22,4 +22,16 @@ public interface ICarState
     */
 
     void UpdateState(CarController car);
+
+
+    /* 
+     Metoda UpdateState - jest wywo³ywana w ka¿dej klatce, gdy samochód znajduje 
+     siê w danym stanie. Przyjmuje jako argument obiekt CarController, który 
+     reprezentuje kontroler samochodu i umo¿liwia dostêp do jego w³aœciwoœci i metod. 
+     Ta metoda jest odpowiedzialna za aktualizacjê stanu, wykonywanie logiki specyficznej 
+     dla tego stanu oraz podejmowanie decyzji dotycz¹cych przejœcia do innych 
+     stanów na podstawie warunków i sytuacji na drodze.
+    */
+
+    void Exit(CarController car);
 }

83d7fa836c4930e4e7128a5464914c0c545ae647
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:26:04 +0100

    metoda Enter wraz z odpowiednim wytłumaczeniem jej działania w komentarzu

--- Assets/Scenes/Scripts/ICarState.cs
+++ Assets/Scenes/Scripts/ICarState.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
 // Piotr Bacior 15 722 - WSEI Kraków - Informatyka stosowana
-public class ICarState
+public interface ICarState
 {
-    
+    /* 
+     Metoda Enter - jest wywo³ywana, gdy samochód wchodzi w dany stan.
+     Przyjmuje jako argument obiekt CarController, który reprezentuje kontroler
+     samochodu i umo¿liwia dostêp do jego w³aœciwoœci i metod.
+     Ta metoda jest odpowiedzialna za inicjalizacjê stanu, ustawienie odpowiednich
+     parametrów i przygotowanie samochodu do dzia³ania w tym stanie. 
+    */
+    void Enter(CarController car);
 }

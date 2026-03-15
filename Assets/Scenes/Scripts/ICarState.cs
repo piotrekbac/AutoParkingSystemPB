9e5070f731837c78be91c5e1af9d6c75986be9b8
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:27:12 +0100

    metoda UpdaetState wraz z odpowiednim wytłumaczeniem jej działania w formie komentarza

--- Assets/Scenes/Scripts/ICarState.cs
+++ Assets/Scenes/Scripts/ICarState.cs
@@ -10,5 +10,16 @@ public interface ICarState
      Ta metoda jest odpowiedzialna za inicjalizacjê stanu, ustawienie odpowiednich
      parametrów i przygotowanie samochodu do dzia³ania w tym stanie. 
     */
+
     void Enter(CarController car);
+
+
+    /* 
+     Metoda Exit - jest wywo³ywana, gdy samochód opuszcza dany stan. Przyjmuje jako 
+     argument obiekt CarController, który reprezentuje kontroler samochodu i umo¿liwia 
+     dostêp do jego w³aœciwoœci i metod. Ta metoda jest odpowiedzialna za czyszczenie 
+     stanu, resetowanie parametrów i przygotowanie samochodu do przejœcia do innego stanu.
+    */
+
+    void UpdateState(CarController car);
 }

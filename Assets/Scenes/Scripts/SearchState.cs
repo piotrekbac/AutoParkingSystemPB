7e3bd84b659d622b3c604c9dae4b24c3e1743910
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-16 00:37:52 +0100

    odebranie graczowi możliwość poruszania się klawiaturą = odpowiednie wartości wejścia pionowego oraz poziomego ruchu wykonywanego przez AI

--- Assets/Scenes/Scripts/SearchState.cs
+++ Assets/Scenes/Scripts/SearchState.cs
@@ -16,6 +16,8 @@ public class SearchState : ICarState
     // Tutaj mo¿na dodaæ logikê, która bêdzie wykonywana podczas tego stanu, np. poruszanie siê po parkingu, skanowanie otoczenia itp.
     public void UpdateState(CarController car)
     {
-        
+        // 1. Odbieramy graczowi klawiaturê - AI bêdzie wciskaæ gaz na 30% mocy i jechaæ prosto
+        car.verticalInput = 0.3f;  // Ustawiamy wartoœæ wejœcia pionowego na 0.3, co oznacza, ¿e samochód bêdzie jecha³ z 30% mocy silnika. 
+        car.horizontalInput = 0f;  // Ustawiamy wartoœæ wejœcia poziomego na 0, co oznacza, ¿e samochód bêdzie jecha³ prosto, bez skrêtu.
     }
 }

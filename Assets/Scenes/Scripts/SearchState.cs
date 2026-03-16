c2c25bddacd198bb9516c71f8dd44bbe07f5b147
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-17 00:27:53 +0100

    inicjalizacja warunku logicznego dotyczącego akcji po znalezieniu miejsca parkinowego - spotFound

--- Assets/Scenes/Scripts/SearchState.cs
+++ Assets/Scenes/Scripts/SearchState.cs
@@ -32,7 +32,13 @@ public class SearchState : ICarState
     // Tutaj mo¿na dodaæ logikê, która bêdzie wykonywana podczas tego stanu, np. poruszanie siê po parkingu, skanowanie otoczenia itp.
     public void UpdateState(CarController car)
     {
-        // 1. Odbieramy graczowi klawiaturê - AI bêdzie wciskaæ gaz na 30% mocy i jechaæ prosto
+        // Je¿eli ju¿ znaleŸliœmy miejsce, nic wiêcej nie robimy - tylko stoimy 
+        if (spotFound)
+        {
+
+        }
+
+        // Odbieramy graczowi klawiaturê - AI bêdzie wciskaæ gaz na 30% mocy i jechaæ prosto
         car.verticalInput = 0.3f;  // Ustawiamy wartoœæ wejœcia pionowego na 0.3, co oznacza, ¿e samochód bêdzie jecha³ z 30% mocy silnika. 
         car.horizontalInput = 0f;  // Ustawiamy wartoœæ wejœcia poziomego na 0, co oznacza, ¿e samochód bêdzie jecha³ prosto, bez skrêtu
 

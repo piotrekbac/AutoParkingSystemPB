9b0125424fbb97151584332a120c7c1a8fcc8f61
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-18 00:45:52 +0100

    Inicjalizacja metody Enter - wywoływana, gdy samochód wchodzi w stan parkowania

--- Assets/Scenes/Scripts/ParkState.cs
+++ Assets/Scenes/Scripts/ParkState.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class ParkState : ICarState
 {
-    
+    // Metoda Enter jest wywo³ywana, gdy samochód wchodzi w stan parkowania. W tej metodzie mo¿na dodaæ logikê, która bêdzie wykonywana podczas tego stanu, np. zatrzymanie samochodu, ustawienie odpowiednich parametrów itp.
+    public void Enter(CarController car)
+    {
+
+    }
 }

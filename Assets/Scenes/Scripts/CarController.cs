56805f67ff2544ce967d7ca6ffa0f993cdb6c35b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-03-22 12:47:23 +0100

    aktualizacja CarController.cs

--- Assets/Scenes/Scripts/CarController.cs
+++ Assets/Scenes/Scripts/CarController.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using System.Collections.Generic; // NOWOŒÆ: Potrzebne do obs³ugi Stosu (Pushdown Automata)
 
 // Piotr Bacior 15 722 - WSEI Kraków - Informatyka stosowana
 
 public class CarController : MonoBehaviour
 {
-    public enum ParkingMode { Parallel, Perpendicular }
+    public enum ParkingMode { Parallel, Perpendicular, Dynamic }
 
     [Header("Tryb Mapy")]
-    public ParkingMode currentMode = ParkingMode.Parallel; [Header("Referencje - Wheel Collidery (Fizyka)")]
+    public ParkingMode currentMode = ParkingMode.Parallel;
+
+    [Header("Referencje - Wheel Collidery")]
     public WheelCollider frontLeftCollider;
     public WheelCollider frontRightCollider;
     public WheelCollider rearLeftCollider;
-    public WheelCollider rearRightCollider; [Header("Referencje - Modele 3D kó³ (Wygl¹d)")]
+    public WheelCollider rearRightCollider;
+
+    [Header("Referencje - Modele 3D")]
     public Transform frontLeftVisual;
     public Transform frontRightVisual;
     public Transform rearLeftVisual;
-    public Transform rearRightVisual; [Header("Ustawienia Pojazdu")]
+    public Transform rearRightVisual;
+
+    [Header("Ustawienia Pojazdu")]
     public Transform centerOfMass;
     public float motorForce = 1500f;
     public float maxSteerAngle = 35f;
@@ -27,7 +34,10 @@ public class CarController : MonoBehaviour
     public float brakeInput; [HideInInspector] public Vector3 targetParkingSpot;
 
     private Rigidbody rb;
-    private ICarState currentState;
+
+    // --- NOWOŒÆ: Pushdown Automata (Automat ze stosem) ---
+    public ICarState currentState;
+    private Stack<ICarState> stateStack = new Stack<ICarState>();
 
     void Start()
     {
@@ -39,6 +49,17 @@ public class CarController : MonoBehaviour
 
     void Update()
     {
+        // GLOBALNY SYSTEM ANTYKOLIZYJNY (Przerwanie HFSM)
+        CarSensors sensors = GetComponent<CarSensors>();
+        if (sensors != null && sensors.isFrontObstacleDetected)
+        {
+            // Jeœli wykryto pieszego, a nie jesteœmy jeszcze w stanie awaryjnym - ZAMRA¯AMY OBECNY STAN!
+            if (!(currentState is EmergencyState))
+            {
+                PushState(new EmergencyState());
+            }
+        }
+
         if (currentState != null) currentState.UpdateState(this);
         UpdateWheelPoses();
     }
@@ -99,17 +120,43 @@ public class CarController : MonoBehaviour
 
     private void UpdateSingleWheel(WheelCollider col, Transform visual)
     {
-        Vector3 pos;
-        Quaternion rot;
-        col.GetWorldPose(out pos, out rot);
+        col.GetWorldPose(out Vector3 pos, out Quaternion rot);
         visual.position = pos;
         visual.rotation = rot;
     }
 
+    // Klasyczna zmiana stanu (czyœci historiê)
     public void ChangeState(ICarState newState)
     {
         if (currentState != null) currentState.Exit(this);
+        stateStack.Clear();
+
         currentState = newState;
+        stateStack.Push(newState);
         currentState.Enter(this);
+    }
+
+    // ZAMRO¯ENIE STANU (K³adzie nowy stan na wierzch stosu)
+    public void PushState(ICarState newState)
+    {
+        currentState = newState;
+        stateStack.Push(newState);
+        currentState.Enter(this);
+    }
+
+    // WZNOWIENIE STANU (Zdejmuje stan awaryjny i wraca do poprzedniego)
+    public void PopState()
+    {
+        if (stateStack.Count > 0)
+        {
+            ICarState oldState = stateStack.Pop();
+            oldState.Exit(this);
+        }
+
+        if (stateStack.Count > 0)
+        {
+            currentState = stateStack.Peek();
+            Debug.Log("FSM [HFSM]: Wznawiam zamro¿ony manewr!");
+        }
     }
 }
